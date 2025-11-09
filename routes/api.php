b290869f74dcd52a94b5d20206c4f733856d1fd3
sabinshrestha8 <sabinshrestha8888@gmail.com>
2025-11-09 21:58:39 +0545

    feat: add courtschedule crud operations apis

--- routes/api.php
+++ routes/api.php
@@ -8,3 +8,8 @@ Route::get('/user', function (Request $request) {
 })->middleware('auth:sanctum');
 
 require __DIR__.'/userapi.php';
+
+Route::middleware('auth:sanctum')->group(function () {
+    // Resource routes for court schedules
+    Route::apiResource('court-schedules', CourtScheduleController::class);
+});
