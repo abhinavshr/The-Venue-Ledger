e792c24b418036dfd98715ca8b0b507b30d640ab
Abhinav Shrestha <abhinavshr002@gmail.com>
2025-11-16 19:16:27 +0545

    refactor: created a separate Auth API and User API

--- routes/userapi.php
+++ routes/userapi.php
@@ -4,12 +4,12 @@ use App\Http\Controllers\Api\User\AuthController;
 use App\Http\Controllers\Api\User\UserController;
 use Illuminate\Support\Facades\Route;
 
-Route::post('/register', [AuthController::class, 'register']);
-Route::post('/login', [AuthController::class, 'login']);
-Route::post('/logout', [AuthController::class, 'logout'])->middleware('auth:sanctum');
-Route::post('/register/{user_id}', [AuthController::class, 'registerFutsalVenue']);
+// Route::post('/register', [AuthController::class, 'register']);
+// Route::post('/login', [AuthController::class, 'login']);
+// Route::post('/logout', [AuthController::class, 'logout'])->middleware('auth:sanctum');
+// Route::post('/register/{user_id}', [AuthController::class, 'registerFutsalVenue']);
 
-Route::middleware('auth:sanctum')->group(function () {
-    Route::get('/profile', [UserController::class, 'getProfile']);
-    Route::put('/profile/update', [UserController::class, 'updateProfile']);
-});
+// Route::middleware('auth:sanctum')->group(function () {
+//     Route::get('/profile', [UserController::class, 'getProfile']);
+//     Route::put('/profile/update', [UserController::class, 'updateProfile']);
+// });
