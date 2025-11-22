56f8e19defe1d0b2119208ff3a3c37f6f2e80781
sabinshrestha8 <sabinshrestha8888@gmail.com>
2025-11-22 08:01:08 +0545

    feat: add court_price_shifts migration with model and controller

---
+++ app/Http/Controllers/Api/CourtPriceShift/CourtPriceShiftController.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Http\Controllers\Api\CourtPriceShift;
+
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+
+class CourtPriceShiftController extends Controller
+{
+    //
+}
